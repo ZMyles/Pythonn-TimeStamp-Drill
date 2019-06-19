ba2a82c2e226bacf2750fc46d59977d4ab948e5e
Zavier <zmyles@comcast.net>
2019-06-19 15:57:39 -0400

    Fixed the file path for listDir() method

--- fileIO.py
+++ fileIO.py
@@ -1,20 +1,44 @@
-
 import os
 import time
 
+"""
+def writeData():
+    data = "Hello World!"
+    with open('Red dragon.txt', 'a') as f:
+        f.write(data)
+        f.close()
 
-files = os.listdir("Python-Drill")
+
+def openFile():
+    with open('Red dragon.txt', 'r') as f:
+        data = f.read()
+        print(data)
+        f.close()
+
+
+
+if __name__ == "__main__":
+##   writeData()
+    openFile()
+"""
+
+##===============
+
+
+files = os.listdir("..\Python-Drill")
+
 
 now = os.getcwd()
 
 for file in files:
+   ## print(os.path.join(now, file))    
     modification_time = os.path.getmtime(file)
-        
 
 local_time = time.ctime(modification_time)
-print("Last modification time: {}".format(local_time))
+print("Last modification time(local time: {})".format(local_time))
 
 
+    
 
 
 
